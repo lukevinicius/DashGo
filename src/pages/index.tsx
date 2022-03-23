3b91b4719bfd292ae81928f284ce24ebca9d8fd9
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-03-23 14:49:41 -0300

    add: SignIn and Dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
